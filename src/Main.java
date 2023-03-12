e4ac21d18f094627916a2374fe34fe4bfb8bf2b9
neto test <ovsepyanm@yahoo.com>
2023-03-13 01:07:07 +0300

    gitignore added

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 2000;
+        int balance = 200;
+        int bonus = 0;
+        if (amount > 1000) {
+            bonus = ( amount / 100 *1 );
+        }
+        int totalBalance = balance + amount + bonus ;
+            System.out.println ( "Количество бонусов: " + bonus );
+        }
+    }
